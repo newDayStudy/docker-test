588185f0a0653e5218620d77dc640e705c445b0d
谷士元 <gushiyuan@163.com>
2023-02-02 12:08:55 +0800

    dafda

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker-test
+
+简单的docker部署
+Dockerfile
+default.conf
+
+
+workflows 工作流模式部署
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# 服务器的脚本
+echo -e "---------docker Login--------"
+docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
+echo -e "---------docker Stop--------"
+docker stop <你镜像的别名(在下面 会生成)>  # 注意修改
+echo -e "---------docker Rm--------"
+docker rm <你容器名字> 									 # 注意修改
+docker rmi <你镜像的名字>									# 注意修改
+echo -e "---------docker Pull--------"
+docker pull <docker服务器上镜像的名字>:<镜像的tag默认是github分支的名字>  		 # 注意修改
+echo -e "---------docker Create and Start--------"
+docker run --rm -d -p 80:80 --name <容器的名字> <docker服务器上镜像的名字>:<镜像的tag默认是github分支的名字> # 注意修改
+echo -e "---------deploy Success--------"
